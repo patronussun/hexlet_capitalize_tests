665fad3baf1195b4321eb5128a99450fb1df0f06
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-04-13 14:51:12 +0300

    update if to assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js'
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
